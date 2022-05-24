6faff4e20b17f6bff661614918ea2f6a4d643c41
umeharaharuki <jasper1116lopez@icloud.com>
2022-05-24 14:50:09 +0900

    ルーティングネスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "cooks#index"
-  resources :cooks
+  resources :cooks do
+  resources :reviews, only: :create
+  end
   resources :users, only: :show
 end
