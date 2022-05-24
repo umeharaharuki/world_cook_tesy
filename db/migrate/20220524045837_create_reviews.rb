610ee4f90145e057c517ed936f023a943d352a9d
umeharaharuki <jasper1116lopez@icloud.com>
2022-05-24 14:10:13 +0900

    reviews Table作成

--- db/migrate/20220524045837_create_reviews.rb
+++ db/migrate/20220524045837_create_reviews.rb
@@ -1,7 +1,10 @@
 class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
-
+      t.integer :star_id,      null: false
+      t.text :comment,         null: false
+      t.references :cook,      null: false, foreign_key: true
+      t.references :user,      null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_20_074034) do
+ActiveRecord::Schema.define(version: 2022_05_24_045837) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -45,6 +45,17 @@ ActiveRecord::Schema.define(version: 2022_05_20_074034) do
     t.index ["user_id"], name: "index_cooks_on_user_id"
   end
 
+  create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "star_id", null: false
+    t.text "comment", null: false
+    t.bigint "cook_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cook_id"], name: "index_reviews_on_cook_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -60,4 +71,6 @@ ActiveRecord::Schema.define(version: 2022_05_20_074034) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "cooks", "users"
+  add_foreign_key "reviews", "cooks"
+  add_foreign_key "reviews", "users"
 end
